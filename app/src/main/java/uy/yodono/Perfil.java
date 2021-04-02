9f4d075f71d31483eaba432766cd3c7cd02df8c3
Shadowpride <60741137+Shadowpride@users.noreply.github.com>
2021-04-02 15:22:35 -0300

    Update Perfil.java

--- app/src/main/java/uy/yodono/Perfil.java
+++ app/src/main/java/uy/yodono/Perfil.java
@@ -6,15 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import uy.yodono.Entidades.Donantes;
 
 public class Perfil extends AppCompatActivity {
 
+    Donantes donante_logueado;
+    TextView nombre;
     Button boton_perfil_editar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        Intent intent = getIntent();
+        Bundle bd = intent.getExtras();
+        donante_logueado = (Donantes)bd.get("Donante");
+        String nombre_donante = donante_logueado.getNombre();
+
+        nombre = (TextView) findViewById(R.id.text_registro_nombre);
+        nombre.setText( nombre_donante );
 
         boton_perfil_editar = (Button)findViewById(R.id.boton_perfil_editar);
 
