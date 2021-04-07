913ae110032243afe5b2e8e8526c5c37dc497233
Matias Nunes <mnunes327@gmail.com>
2021-04-07 20:07:26 -0300

    uso de viewmodel en ListaDonantes

--- app/src/main/java/uy/yodono/ListaDonantes.java
+++ app/src/main/java/uy/yodono/ListaDonantes.java
@@ -1,16 +1,30 @@
 package uy.yodono;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import uy.yodono.BD.AppDatabase;
+import uy.yodono.Entidades.Donantes;
+import uy.yodono.daos.DonanteDao;
+
 public class ListaDonantes extends AppCompatActivity {
+
+    Intent intent;
+    Donantes donante_logueado;
+
+    private DonantesViewModel donantesViewModel;
 
     List<ListElementListaDonantes> elements;
 
@@ -18,31 +32,27 @@ public class ListaDonantes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_donantes);
-        init();
-    }
 
-    public void init() {
-        elements = new ArrayList<>();
-        elements.add(new ListElementListaDonantes("Juan Carlos Perez","B+","Montevideo"));
-        elements.add(new ListElementListaDonantes("Maria Rodriguez","O+","Rocha"));
-        elements.add(new ListElementListaDonantes("Carlos Ramirez","B+","Canelones"));
-        elements.add(new ListElementListaDonantes("Juan Carlos Perez","B+","Montevideo"));
-        elements.add(new ListElementListaDonantes("Maria Rodriguez","O+","Rocha"));
-        elements.add(new ListElementListaDonantes("Carlos Ramirez","B+","Canelones"));
-        elements.add(new ListElementListaDonantes("Juan Carlos Perez","B+","Montevideo"));
-        elements.add(new ListElementListaDonantes("Carlos Ramirez","B+","Canelones"));
-        elements.add(new ListElementListaDonantes("Juan Carlos Perez","B+","Montevideo"));
-        elements.add(new ListElementListaDonantes("Maria Rodriguez","O+","Rocha"));
-        elements.add(new ListElementListaDonantes("Carlos Ramirez","B+","Canelones"));
-        elements.add(new ListElementListaDonantes("Juan Carlos Perez","B+","Montevideo"));
+        intent = getIntent();
+        Bundle bd = intent.getExtras();
+        donante_logueado = (Donantes) bd.get("Donante");
 
-
-        ListAdapterDonantes listAdapterDonantes = new ListAdapterDonantes(elements,ListaDonantes.this);
         RecyclerView recyclerView = findViewById(R.id.ListRecyclerViewBuscarDonantes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(ListaDonantes.this, 2));
-        recyclerView.setAdapter(listAdapterDonantes);
+        DonantesAdapter adapter = new DonantesAdapter();
+        recyclerView.setAdapter(adapter);
 
+        donantesViewModel = new ViewModelProvider(this,
+                ViewModelProvider.AndroidViewModelFactory
+                        .getInstance(this.getApplication()))
+                .get(DonantesViewModel.class);
+
+        donantesViewModel.getListaOtrosDonantes( donante_logueado.getCi() ).observe(this, new Observer<List<Donantes>>() {
+            @Override
+            public void onChanged(List<Donantes> donantes) {
+                adapter.setLista_donantes(donantes);
+            }
+        });
     }
-
 }
