65234edb142856c47d2d711e14e41d5eb64eab9e
Shadowpride <60741137+Shadowpride@users.noreply.github.com>
2021-04-08 19:12:26 -0300

    Update PerfilEditar.java

--- app/src/main/java/uy/yodono/PerfilEditar.java
+++ app/src/main/java/uy/yodono/PerfilEditar.java
@@ -1,27 +1,68 @@
 package uy.yodono;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class PerfilEditar extends AppCompatActivity {
 
     Button boton_perfil_aplicar;
     Button boton_perfil_cancelar;
 
+    EditText text_cedula;
+    EditText text_contrasena;
+    EditText text_contrasena_confirmacion;
+    EditText text_nombre;
+    EditText text_apellido;
+    EditText text_email;
+    EditText text_telefono;
+
+    private DonantesViewModel donantesViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_editar);
+
+        donantesViewModel = new ViewModelProvider( this,
+                ViewModelProvider.AndroidViewModelFactory
+                        .getInstance(this.getApplication()))
+                .get(DonantesViewModel.class);
+
+        Spinner spinner_departamentos = (Spinner) findViewById(R.id.spinner_departamentos);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_departamentos = ArrayAdapter.createFromResource(this,
+                R.array.array_departamentos, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter_departamentos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner_departamentos.setAdapter(adapter_departamentos);
+
+        Spinner spinner_grupos_sanguineos = (Spinner) findViewById(R.id.spinner_grupo_sanguineos);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter_grupos_sanguineos = ArrayAdapter.createFromResource(this,
+                R.array.array_grupos_sanguineos, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter_grupos_sanguineos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner_grupos_sanguineos.setAdapter(adapter_grupos_sanguineos);
 
         boton_perfil_aplicar = (Button)findViewById(R.id.boton_perfil_aplicar);
 
         boton_perfil_aplicar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+
+
                 Intent i = new Intent(PerfilEditar.this, Perfil.class);
                 startActivity(i);
             }
