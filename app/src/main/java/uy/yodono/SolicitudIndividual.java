b3ee67eacad78ddc2bca05b9836b4ed0855c6562
camimbenedetti <camilambenedetti@gmail.com>
2021-04-16 00:15:36 -0300

    List Element Mis Solicitudes + Layout SolicitudIndividual

---
+++ app/src/main/java/uy/yodono/SolicitudIndividual.java
@@ -0,0 +1,14 @@
+package uy.yodono;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SolicitudIndividual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitud_individual);
+    }
+}
